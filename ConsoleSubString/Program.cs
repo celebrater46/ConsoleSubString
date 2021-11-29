42887efef3308f3b56d52007348aedd922b3d62f
hideru <jota.16.stigma.world@gmail.com>
2021-11-29 20:30:24 +0900

    test succeeded

--- ConsoleSubString/Program.cs
+++ ConsoleSubString/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System;
+
+// Console.WriteLine("Hello, World!");
+class Sample
+{
+    static void Main()
+    {
+        string str = "第三世界収容所";
+        Console.WriteLine(str.Substring(0, 1));
+        Console.WriteLine(str.Substring(1, 5));
+        Console.WriteLine(str.Substring(6));
+        // Console.ReadKey();
+        
+        string src_str = "白金グループ";
+        // string target_str = "白金"; // 白金
+        string target_str = "グループ"; // グループ
+        // Console.WriteLine(src_str.Substring(src_str.IndexOf(target_str), target_str.Length)); 
+        Console.WriteLine(src_str.Substring(0, src_str.IndexOf(target_str))); 
+        // Console.WriteLine(src_str.Substring(2, target_str.Length)); 
+        Console.ReadKey();
+    }
+}
